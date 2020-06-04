35811f94a96c1c3cb9678221392b15c65cd85f2e
balmanu84 <bal.manu84@gmail.com>
2020-06-04 19:28:50 +0530

    New Helloworld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-      System.out.println("Hello World program from Git to GitHub then Jenkins");
+      System.out.println("Hello World From Jenkins");
 	}
 
 }
